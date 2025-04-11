a5820a5cf52687ddc7148e2608d81d9315450b8f
Michal A Uchmanowicz <mike.uchmanowicz@gmail.com>
2025-04-10 21:46:18 -0700

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,31 +1,31 @@
-<script>
-    document.addEventListener('DOMContentLoaded', () => {
-        const modals = document.querySelectorAll('.modal');
-        const body = document.querySelector('body');
 
-        function showModal(id) {
-            modals.forEach(modal => {
-                modal.classList.remove('visible');
-            });
-            document.getElementById(id).classList.add('visible');
-            body.classList.add('modal-open');
-        }
+document.addEventListener('DOMContentLoaded', () => {
+    const modals = document.querySelectorAll('.modal');
+    const body = document.querySelector('body');
 
-        function closeModal() {
-            modals.forEach(modal => modal.classList.remove('visible'));
-            body.classList.remove('modal-open');
-        }
-
-        // Close modals on clicking outside
+    function showModal(id) {
         modals.forEach(modal => {
-            modal.addEventListener('click', (e) => {
-                if (e.target === modal) closeModal();
-            });
+            modal.classList.remove('visible');
         });
+        document.getElementById(id).classList.add('visible');
+        body.classList.add('modal-open');
+    }
 
-        // Close modals with 'Esc' key
-        document.addEventListener('keydown', (e) => {
-            if (e.key === 'Escape') closeModal();
+    function closeModal() {
+        modals.forEach(modal => modal.classList.remove('visible'));
+        body.classList.remove('modal-open');
+    }
+
+    // Close modals on clicking outside
+    modals.forEach(modal => {
+        modal.addEventListener('click', (e) => {
+            if (e.target === modal) closeModal();
         });
     });
-</script>
+
+    // Close modals with 'Esc' key
+    document.addEventListener('keydown', (e) => {
+        if (e.key === 'Escape') closeModal();
+    });
+});
+
